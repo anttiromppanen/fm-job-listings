2a85c221489af571ba8118f9f35af35fc070085e
Antti Romppanen <romppanen.antti@gmail.com>
2024-08-23 12:38:12 +0300

    added animations for filter bar

---
+++ src/components/FilterBar/FilterItem.tsx
@@ -0,0 +1,43 @@
+import { AnimatePresence, motion, Variants } from "framer-motion";
+import useFilterStore from "@/store/useFilterStore";
+import Image from "next/image";
+
+const animationVariants: Variants = {
+  hidden: { x: -10, opacity: 0 },
+  visible: { x: 0, opacity: 1 },
+  exit: { x: -10, opacity: 0 },
+};
+
+function FilterItem({ item }: { item: string }) {
+  const setFilter = useFilterStore((state) => state.setFilter);
+  const handleRemoveFilter = () => setFilter(item);
+
+  return (
+    <motion.div
+      variants={animationVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      className="bg-userFilterTablets text-xs rounded-md flex items-center"
+    >
+      <span className="text-userDesaturatedDarkCyan px-2 pt-1 font-bold cursor-pointer">
+        {item}
+      </span>
+      <button
+        type="button"
+        aria-label={`Remove ${item} from filter`}
+        onClick={handleRemoveFilter}
+        className="buttons-hover-anim bg-userDesaturatedDarkCyan flex items-center justify-center p-2 rounded-r-md hover:bg-userVeryDarkGrayishCyan"
+      >
+        <Image
+          src="/img/icon-remove.svg"
+          alt="Remove icon"
+          width={10}
+          height={10}
+        />
+      </button>
+    </motion.div>
+  );
+}
+
+export default FilterItem;
--- src/components/FilterBar/index.tsx
+++ src/components/FilterBar/index.tsx
@@ -1,53 +1,50 @@
 "use client";
 
-import Image from "next/image";
+import { AnimatePresence, motion, Variants } from "framer-motion";
 import useFilterStore from "@/store/useFilterStore";
+import FilterItem from "./FilterItem";
 
-function FilterItem({ item }: { item: string }) {
-  const setFilter = useFilterStore((state) => state.setFilter);
-  const handleRemoveFilter = () => setFilter(item);
-
-  return (
-    <div className="bg-userFilterTablets text-xs rounded-md flex items-center">
-      <span className="text-userDesaturatedDarkCyan px-2 pt-1 font-bold cursor-pointer">
-        {item}
-      </span>
-      <button
-        type="button"
-        aria-label={`Remove ${item} from filter`}
-        onClick={handleRemoveFilter}
-        className="buttons-hover-anim bg-userDesaturatedDarkCyan flex items-center justify-center p-2 rounded-r-md hover:bg-userVeryDarkGrayishCyan"
-      >
-        <Image
-          src="/img/icon-remove.svg"
-          alt="Remove icon"
-          width={10}
-          height={10}
-        />
-      </button>
-    </div>
-  );
-}
+const animationVariants: Variants = {
+  hidden: { opacity: 0, y: -10, transition: { duration: 0.2 } },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.2, type: "spring", stiffness: 50 },
+  },
+  exit: { opacity: 0, y: -10, transition: { duration: 0.2 } },
+};
 
 function FilterBar() {
   const filter = useFilterStore((state) => state.filter);
   const clearFilter = useFilterStore((state) => state.clearFilter);
 
   return (
-    <div className="card-shadow bg-white mx-auto flex justify-between max-w-6xl -mt-7 px-10 py-4 rounded-md">
-      <div className="flex gap-x-4">
-        {filter.map((item) => (
-          <FilterItem key={item} item={item} />
-        ))}
-      </div>
-      <button
-        type="button"
-        onClick={clearFilter}
-        className="buttons-hover-anim text-userDarkGrayishCyan text-sm font-bold hover:underline hover:text-userDesaturatedDarkCyan p-1"
-      >
-        Clear
-      </button>
-    </div>
+    <AnimatePresence>
+      {filter.length && (
+        <motion.div
+          variants={animationVariants}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          className="card-shadow bg-white mx-auto flex justify-between max-w-6xl -mt-7 px-10 py-4 rounded-md"
+        >
+          <div className="flex gap-x-4">
+            <AnimatePresence mode="sync">
+              {filter.map((item) => (
+                <FilterItem key={item} item={item} />
+              ))}
+            </AnimatePresence>
+          </div>
+          <button
+            type="button"
+            onClick={clearFilter}
+            className="buttons-hover-anim text-userDarkGrayishCyan text-sm font-bold hover:underline hover:text-userDesaturatedDarkCyan p-1"
+          >
+            Clear
+          </button>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
 
